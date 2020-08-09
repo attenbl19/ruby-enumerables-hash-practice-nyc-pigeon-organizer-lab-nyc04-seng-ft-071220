1d6f1e5f22ef0ff37a7d9fb7a63064162a1a526d
Lida Attenborough <attenboroughl@protonmail.com>
2020-08-09 16:10:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  nyc_pigeon_organizer = pigeon_list
+  pigeon_list = {}
+  #Step 1 - create a new hash and store the names as the key
+   pigeon_data.each_with_object({}) do |(top_keys, top_keys_values), pigeon_list|
+       top_keys_values.each do |color_keys, names|
+         names.each do |name|
+           if !pigeon_list =
+         end
+       end
+    end
+  pigeon_list
 end
